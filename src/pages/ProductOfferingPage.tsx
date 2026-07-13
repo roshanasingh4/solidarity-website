3bc6784a019363c88698bd8b3d66fa7a73911bb0
Roshan Singh <roshanasingh4@gmail.com>
2026-07-13 13:53:34 +0530

    Update latest investor presentation

--- src/pages/ProductOfferingPage.tsx
+++ src/pages/ProductOfferingPage.tsx
@@ -43,7 +43,7 @@ export default function ProductOfferingPage() {
           <p className="product-offering-presentation">
             Please click{' '}
             <a
-              href="/wp-content/uploads/2026/06/Introduction-to-Solidarity-5-Jun-2026-V1-1.pdf"
+              href="/wp-content/uploads/2026/07/Introduction-to-Solidarity-9-Jul-2026.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="presentation-link"
