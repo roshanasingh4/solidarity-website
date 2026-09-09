de9c02c80853e3b141f3163e643ca778adca624e
Roshan Singh <roshanasingh4@gmail.com>
2026-09-09 09:47:46 +0530

    Update PMS investor presentation for September

--- src/pages/ProductOfferingPage.tsx
+++ src/pages/ProductOfferingPage.tsx
@@ -43,7 +43,7 @@ export default function ProductOfferingPage() {
           <p className="product-offering-presentation">
             Please click{' '}
             <a
-              href="/wp-content/uploads/2026/08/Introduction-to-Solidarity-7-Aug-2026.pdf"
+              href="/wp-content/uploads/2026/09/Introduction-to-Solidarity-8-Sep-2026.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="presentation-link"
